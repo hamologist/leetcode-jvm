e9a61e935f9177af39b0f216ad9821f3b550ac54
Jordan Paris <hamologist@gmail.com>
2021-09-03 00:19:47 -0400

    Additional java solutions

---
+++ src/com/hamologist/leetcode/easy/permutations/Main.java
@@ -0,0 +1,53 @@
+package com.hamologist.leetcode.easy.permutations;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        List<List<Integer>> result = solution.permute(new int[] {1, 2, 3});
+        System.out.println(result);
+    }
+}
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        new Inner();
+        return Inner.solve(nums);
+    }
+
+    static private class Inner {
+        final List<List<Integer>> result;
+
+        Inner() {
+            result = new ArrayList<>();
+        }
+
+        void permute(List<Integer> current, List<Integer> nums) {
+            if (nums.isEmpty()) {
+                result.add(current);
+                return;
+            }
+
+            int index = 0;
+            for (int num : nums) {
+                ArrayList<Integer> nextCurrent = new ArrayList<>(current);
+                ArrayList<Integer> nextNums = new ArrayList<>(nums);
+                nextCurrent.add(num);
+                nextNums.remove(index);
+                permute(nextCurrent, nextNums);
+                index++;
+            }
+        }
+
+        static List<List<Integer>> solve(int[] nums) {
+            final Inner inner = new Inner();
+            inner.permute(new ArrayList<>(), Arrays.stream(nums).boxed().collect(Collectors.toList()));
+
+            return inner.result;
+        }
+    }
+}
